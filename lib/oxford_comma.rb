108341f2ee4e5dbf57383a1650a857d18cde9b5c
Jonathan Hong <jonhong777@gmail.com>
2018-07-03 16:21:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,11 @@ def oxford_comma(array)
       return array[0]
     when 2
       return array.join(" and ")
-  string = array.join(", ")
-  new_array = string.split(" ")
-  length = new_array.length
-  new_array.insert(length - 1, "and")
-  new_array = new_array.join(" ")
+    else
+      string = array.join(", ")
+      new_array = string.split(" ")
+      length = new_array.length
+      new_array.insert(length - 1, "and")
+      new_array = new_array.join(" ")
+  end
 end
